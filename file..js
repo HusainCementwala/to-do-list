397618e7078680b25d88b5194c6875938410c14b
Husain <husaincementwalas@gmail.com>
2025-01-05 12:38:21 +0530

    css better required

--- file..js
+++ file..js
@@ -1,29 +1,44 @@
 const todoList = [];
 
-
-
-
 function renderTodoList(){
 
 let todoListHTML = '';
 
 for(let i=0 ; i<todoList.length ; i++){
-const todo = todoList[i];
-const html = `<p>${todo}</p>`;
+const todoObject = todoList[i];
+const name = todoObject.name; // const { name, dueDate } = todoObject
+const date = todoObject.dueDate;
+const html = `
+<div>${name}</div>
+<div>${date}</div> 
+<button onclick="
+todoList.splice(${i},1);
+renderTodoList();
+" class="delete-button">Delete</button>`;
+
 todoListHTML += html ;
 }
 
 document.querySelector('.js-todo-list') /* we put <p> from here as we cannot put a para in div directly in html*/
-
 .innerHTML = todoListHTML;
 }
+
 
 
 function addTodo(){
 
   const inputElement = document.querySelector('.js-input');
-  const name =inputElement.value;
-  todoList.push(name);
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+
+  todoList.push({
+   name : name,
+   dueDate :dueDate  
+    
+  });
   
 inputElement.value = '';
 renderTodoList();
